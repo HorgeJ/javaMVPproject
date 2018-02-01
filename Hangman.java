43b210feebef2c54a41492befabc254781c45005
Jorge Sagastume <HorgeJ@users.noreply.github.com>
2018-02-01 12:12:02 -0800

    Added prompter class and implemented prompting method for user

--- Hangman.java
+++ Hangman.java
@@ -1,5 +1,14 @@
 public class Hangman{
     public static void main(String[] args){
         Game game = new Game("treehouse");          // Creating a new instance of our Game class called Game with answer passed in
+        Prompter prompter = new Prompter(game);         // to use prompter with oyur game passed in
+
+        boolean isHit = prompter.promptForGuess();
+
+        if(isHit){
+            System.out.println("We got a Hit!");
+        } else {
+            System.out.println("Oops missed");
+        }
     }
 }
--- Prompter.java
+++ Prompter.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner; // lets us take in imput
+
 public class Prompter {
+    //Front end portion of app
+    
+    private Game game;          // Letting the propter class know about our Game class
+
+    public Prompter(Game game){     // Constructor that refrences our game 
+        this.game = game;           // set our private Game = to the game parameter passed in
+    }
+
+    public boolean promptForGuess(){    //Will prompt for guess and determine if it was correct
+        Scanner scanner = new Scanner(System.in);       // new instance of scanner for us to use
+        System.out.print("Enter a letter:  ");          // prompt for input
+        String guessInput = scanner.nextLine();
+        char guess = guessInput.charAt(0);              // take char at position 0
+        return game.applyGuess(guess);                  // sending our char to the game class and invoking applyGuess function on guess char
+    }
     
 }
